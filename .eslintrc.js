de93c5bb781f46a1bb46f79c6ee289a0f3bc855d
Riad Benguella <benguella@gmail.com>
2025-11-27 12:27:10 +0100

    Linting: Disable the wordpress dependency group lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
-/**
- * External dependencies
- */
-const glob = require( 'glob' ).sync;
 const { join } = require( 'path' );
+const glob = require( 'glob' ).sync;
 
 /**
  * The list of patterns matching files used only for development purposes.
@@ -155,7 +152,6 @@ module.exports = {
 			'error',
 			{ props: 'never', children: 'never' },
 		],
-		'@wordpress/dependency-group': 'error',
 		'@wordpress/wp-global-usage': 'error',
 		'@wordpress/react-no-unsafe-timeout': 'error',
 		'@wordpress/i18n-hyphenated-range': 'error',
