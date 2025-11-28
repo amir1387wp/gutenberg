2692a61e641606805fc9c025a06f6f44ac8161e8
Jarda Snajdr <jsnajdr@gmail.com>
2025-11-28 14:09:42 +0100

    Storybook: skip transpilation of build-modules files

--- storybook/main.js
+++ storybook/main.js
@@ -74,12 +74,37 @@ module.exports = {
 		reactDocgen: 'react-docgen-typescript',
 	},
 	webpackFinal: async ( config ) => {
+		// Find the `babel-loader` rule added by `@storybook/addon-webpack5-compiler-babel`
+		// and add exclude for `packages/*/build-module` folders.
+		const rules = config.module.rules.map( ( rule ) => {
+			const usesBabelLoader =
+				Array.isArray( rule.use ) &&
+				rule.use.some(
+					( loader ) =>
+						typeof loader === 'object' &&
+						loader.loader &&
+						loader.loader.includes( 'babel-loader' )
+				);
+
+			if ( usesBabelLoader ) {
+				// Add exclude for packages/*/build-module folders
+				const buildModuleExclude = /packages\/[^/]+\/build-module/;
+				if ( Array.isArray( rule.exclude ) ) {
+					return {
+						...rule,
+						exclude: [ ...rule.exclude, buildModuleExclude ],
+					};
+				}
+			}
+			return rule;
+		} );
+
 		return {
 			...config,
 			module: {
 				...config.module,
 				rules: [
-					...config.module.rules,
+					...rules,
 					{
 						test: /\.md$/,
 						type: 'asset/source',
